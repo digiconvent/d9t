ad4ffaaf1af8fa103ae9bc5ce6f8716fbb95aad5
digiconvent <digiconvent1815@gmail.com>
2025-09-21 15:29:05 +0200

    policies always have at least an empty description

--- backend/pkg/iam/db/0.0.0/006_create_policies_table.sql
+++ backend/pkg/iam/db/0.0.0/006_create_policies_table.sql
@@ -1,6 +1,6 @@
 create table policies (
    id uuid primary key,
    name text not null,
-   description text,
+   description text not null default "",
    votes_required integer not null default 1
 );
