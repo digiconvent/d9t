2999f23e1814c56c36701c12b71ec33ab3f69888
digiconvent <digiconvent1815@gmail.com>
2025-09-21 15:31:30 +0200

    implement read_permissions

--- backend/pkg/iam/repo/policy/read_permissions.go
+++ backend/pkg/iam/repo/policy/read_permissions.go
@@ -6,6 +6,27 @@ import (
 )
 
 func (r *policyRepository) ReadPermissions(id *uuid.UUID) ([]string, *core.Status) {
-	permissions := []string{}
-	return permissions, nil
+	query := `select permission from policy_has_permission where policy = ?`
+
+	rows, err := r.db.Query(query, id.String())
+	if err != nil {
+		return nil, core.InternalError("failed to read policy permissions: " + err.Error())
+	}
+	defer rows.Close()
+
+	var permissions []string
+	for rows.Next() {
+		var permission string
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, core.InternalError("failed to scan policy permission")
+		}
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, core.InternalError("failed to iterate policy permissions")
+	}
+
+	return permissions, core.StatusSuccess()
 }
